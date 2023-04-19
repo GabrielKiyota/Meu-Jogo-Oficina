1d2639ec583f1e8b827ce8e2d62feb2390a03bb1
Pedro Pegado <gabrielkiyota@gmail.com>
2023-04-19 15:14:31 -0300

    adicionei o mario

--- Meu Jogo OficinaYuji/Assets/Programação Jogo/Personagem.cs
+++ Meu Jogo OficinaYuji/Assets/Programação Jogo/Personagem.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
-
     public int velocidade;
-    
-            
+    public int forcaPulo;
+    public bool noAr;
+    public SpriteRenderer visual;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        noAr = false;
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        noAr = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        //pegando input do teclado
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
 
-        //imprimindo valores no console
-        //Debug.Log("Horizontal: " + x); 
-        //Debug.Log("Vertical: " + y);
-
-        Vector3 inputTeclado = new Vector3(x, y, 0);
+        Vector3 inputTeclado = new Vector3(x, 0, 0);
         transform.position += inputTeclado * velocidade * Time.deltaTime;
 
+
+        if (Input.GetKeyDown(KeyCode.Space) && !noAr)
+        {
+            Debug.Log("PRESSIONOU");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+        }
+        
+        if(x < 0)
+        {
+            //olhando pra esquerda
+            visual.flipX = true;
+        }
+        else if(x > 0)
+        {
+            //olhando pra direita
+            visual.flipX = false;
+        }
     }
 }
