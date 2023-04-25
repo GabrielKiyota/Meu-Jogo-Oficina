d746322d887fe604c93958d9471e4ec16d14497b
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-04-24 23:46:37 -0300

    AAAAAA ZÉ DA MANGA

--- Meu Jogo OficinaYuji/Assets/Programação Jogo/Personagem.cs
+++ Meu Jogo OficinaYuji/Assets/Programação Jogo/Personagem.cs
@@ -4,48 +4,32 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
-    public int velocidade;
-    public int forcaPulo;
-    public bool noAr;
-    public SpriteRenderer visual;
+
+    public Animator anim;
+    public float Speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        noAr = false;
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        noAr = true;
-    }
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-
-        Vector3 inputTeclado = new Vector3(x, 0, 0);
-        transform.position += inputTeclado * velocidade * Time.deltaTime;
-
-
-        if (Input.GetKeyDown(KeyCode.Space) && !noAr)
-        {
-            Debug.Log("PRESSIONOU");
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
-        }
+        //pegando input do teclado
         
-        if(x < 0)
-        {
-            //olhando pra esquerda
-            visual.flipX = true;
-        }
-        else if(x > 0)
-        {
-            //olhando pra direita
-            visual.flipX = false;
-        }
+        
+        //imprimindo valores no console
+        //Debug.Log("Horizontal: " + x); 
+        //Debug.Log("Vertical: " + y);
+
+        
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0f);
+        
+        anim.SetFloat("Horizontal",movimento.x);
+        anim.SetFloat("Vertical",movimento.y);
+        anim.SetFloat("Speed",movimento.magnitude);
+
+        transform.position = transform.position + movimento * Speed * Time.deltaTime;
     }
 }
